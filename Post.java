dbc24e462e45eb3562f284608efabfde0560c143
shivam <shivamrawat1053@gmail.com>
2024-05-09 18:24:55 +0530

    p

---
+++ Post.java
@@ -0,0 +1,16 @@
+class Post
+{
+public static void main(String []args)
+{
+int a,b;
+a=10;
+b=a++;
+System.out.println("A:"+a);
+System.out.println("B:"+b);
+a=10;
+b=a--;
+System.out.println("A:"+a);
+System.out.println("B:"+b);
+}
+}
+
---
+++ Pre.java
@@ -0,0 +1,15 @@
+class Pre 
+{
+public static void main(String []args)
+{
+int a,b;
+a=10;
+b=++a;
+System.out.println("A:"+a);
+System.out.println("B:"+b);
+a=10;
+b=--a;
+System.out.println("A:"+a);
+System.out.println("B:"+b);
+}
+}
