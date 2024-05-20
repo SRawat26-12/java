8f9270ce4ce558eadc60f6e163ac9142085750da
shivam <shivamrawat1053@gmail.com>
2024-05-21 02:48:27 +0530

    javaprogram

---
+++ Divisible.java
@@ -0,0 +1,15 @@
+class Divisible
+{
+public static void main(String []args)
+{
+int a=50;
+int b=100;
+for(a=50;a<=100;a++)
+{
+if(a%5==0)
+{
+System.out.println(a+" ");
+}
+}
+}
+}
---
+++ Dvisible1.java
@@ -0,0 +1,13 @@
+class Dvisible1
+{
+public static void main(String []args)
+{
+for(int i=1;i<=60;i++)
+{
+if(i%5==0&&i%3==0)
+{
+System.out.println(i+" ");
+}
+}
+}
+}
