4ee80831458688b3b0af414925001eff4ed9de65
shivam <shivamrawat1053@gmail.com>
2024-06-07 18:31:11 +0530

    String

---
+++ S1.java
@@ -0,0 +1,12 @@
+class S1
+{
+public static void main(String []args)
+{
+String str=" hello world ";
+String a=str.trim();
+System.out.println(a);
+int s=1234;
+String c=String.valueOf(s);
+System.out.println(c);
+}
+}
